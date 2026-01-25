02dfe28e4128e33225f0fe8ca51b6c8789d69594
lavishdev <saini.lavish2007@gmail.com>
2026-01-25 18:56:31 +0530

    feat: initial Spring Boot project setup

---
+++ src/test/java/com/lavish/jobportalbackend/JobPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavish.jobportalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
